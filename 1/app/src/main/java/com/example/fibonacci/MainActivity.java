4e5e78a4bcc3ecc0fee739e29945c40dde037b9d
Diego Mateus <diego.mateus@javeriana.edu.co>
2020-02-05 00:15:24 -0500

    Done the four point of the activity

--- 1/app/src/main/java/com/example/fibonacci/MainActivity.java
+++ 1/app/src/main/java/com/example/fibonacci/MainActivity.java
@@ -1,54 +1,50 @@
 package com.example.fibonacci;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
-    TextView textView;
-    String serie;
-    StringTokenizer token;
-    int nDatos;
-    int answer;
-    int[] datos;
-    int a;
+    LinearLayout linearLayout;
+    ArrayList<Long> numbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         button = findViewById(R.id.button);
+        linearLayout = findViewById(R.id.linearLayout);
+        numbers = new ArrayList<>();
+        numbers.add(Long.parseLong("0"));
+        numbers.add(Long.parseLong("1"));
 
         button.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
 
-                textView = findViewById(R.id.textView);
-                serie = textView.getText().toString();
-                token = new StringTokenizer(serie, ",");
-                nDatos = token.countTokens();
-                datos = new int[1000];
-                a = 0;
+                TextView newText;
+                newText = new TextView(view.getContext());
 
-                for(int i = 0; token.hasMoreTokens(); i++){
-                    String str = token.nextToken();
-                    datos[i] = Integer.valueOf(str);
-                    a = i;
-                }
+                long numberOne = numbers.get(numbers.size() - 1);
+                long numberTwo = numbers.get(numbers.size() - 2);
+                long answer = numberOne + numberTwo;
 
-                answer = datos[a] + datos[a-1];
+                String respuesta = String.valueOf(answer);
 
-                textView.setText(serie + "," + answer);
+                newText.setText(respuesta);
+                linearLayout.addView(newText);
+
+                numbers.add(answer);
+
             }
         });
     }
